c35960d2e65d061b58cf93f2e2a9841d8f489154
Piano CMoll <piano.cmoll@gmail.com>
2026-01-31 21:20:05 +0100

    Add receive email

--- tests/e2e/tc01.spec.ts
+++ tests/e2e/tc01.spec.ts
@@ -35,14 +35,6 @@ test.describe('Home Page E2E Tests', () => {
 
     });
 
-    test('test case 2', async ({ page }) => {
-        console.log('test case 2');
-
-    });
-
-    test('test case 3', async ({ page }) => {
-        console.log('test case 3');
-    });
 
     // Add more test cases as needed
 });
