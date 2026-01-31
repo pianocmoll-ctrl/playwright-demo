2cd6738bddb52e64f3e53af1d1fbff39c1f40c29
Piano CMoll <piano.cmoll@gmail.com>
2026-01-31 17:56:46 +0100

    add wating timeout

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,7 +4,7 @@ import { Page } from "playwright";
 
 export async function doScreenshot(screenShotName: string, testInfo: any, page: Page) {
 
-    page.waitForTimeout(1500);
+    page.waitForTimeout(2000);
     const elementPath = testInfo.outputPath(screenShotName + '.png');
     await page.locator('form').screenshot({ path: elementPath });
     // attach the screenshot to the test so the HTML report shows it
