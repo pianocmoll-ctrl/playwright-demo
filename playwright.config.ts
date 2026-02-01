97c98a6269e1c300eab7b5f2fb8ea2f3c7f0c6d2
Piano CMoll <piano.cmoll@gmail.com>
2026-02-01 18:57:06 +0100

    run with browser headless = false

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   use: {
     actionTimeout: 15000,
     navigationTimeout: 180000,
-    headless: true,
+    headless: false,
     browserName: 'chromium',
     acceptDownloads: true,
     trace: 'on-first-retry',
